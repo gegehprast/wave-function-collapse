a77a255c7b182a74673d47a5dec98b6c8e3ea06a
gegehprast <36652820+gegehprast@users.noreply.github.com>
2024-01-16 07:23:25 +0700

    Refactor Cell class to use history array for saving and resetting state

--- src/Cell.ts
+++ src/Cell.ts
@@ -7,29 +7,53 @@ class Cell {
     tile?: Tile
     collapsed: boolean = false
 
-    lastState: { possibleTiles: Tile[]; tile?: Tile; collapsed: boolean }
+    history: { possibleTiles: Tile[]; tile?: Tile; collapsed: boolean }[] = []
 
     constructor(x: number, y: number, tiles: Tile[]) {
         this.x = x
         this.y = y
         this.possibleTiles = tiles
 
-        this.lastState = { possibleTiles: this.possibleTiles, tile: this.tile, collapsed: this.collapsed }
+        this.history.push({ possibleTiles: this.possibleTiles, tile: this.tile, collapsed: this.collapsed })
     }
 
     save() {
-        this.lastState = { possibleTiles: this.possibleTiles, tile: this.tile, collapsed: this.collapsed }
+        this.history.push({ possibleTiles: this.possibleTiles, tile: this.tile, collapsed: this.collapsed })
     }
 
-    reset() {
-        this.possibleTiles = this.lastState.possibleTiles
-        this.tile = this.lastState.tile
-        this.collapsed = this.lastState.collapsed
+    reset(lastNum?: number) {
+        if (!lastNum) {
+            const last = this.history.pop()
+
+            if (last) {
+                this.possibleTiles = last.possibleTiles
+                this.tile = last.tile
+                this.collapsed = last.collapsed
+            }
+
+            return
+        }
+
+        const index = this.history.length - lastNum - 1
+        
+        if (index < 0) {
+            return
+        }
+
+        const last = this.history[index]
+
+        if (last) {
+            this.possibleTiles = last.possibleTiles
+            this.tile = last.tile
+            this.collapsed = last.collapsed
+        }
+
+        this.history = this.history.slice(0, index + 1)
     }
 
     collapse(tile: Tile) {
         this.save()
-        
+
         this.tile = tile
         this.collapsed = true
         this.possibleTiles = [this.tile]
